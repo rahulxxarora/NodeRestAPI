8e2fda958c260da4ef24279351a50f072d1b0bdb
Rahul Arora <rahulxxarora@gmail.com>
2018-02-17 20:26:50 +0530

    Code refactored

--- routes/notes.js
+++ routes/notes.js
@@ -4,7 +4,6 @@ const notesDomain = require('../domain/notes');
 
 exports.createNote = function (req, res, next) {
     let noteData = req.body;
-    console.log(noteData);
     notesDomain.createNote(noteData)
         .then(note => {
             res.send(note)
--- tools/db_tools.js
+++ tools/db_tools.js
@@ -11,9 +11,11 @@ exports.DBConnectMongoose = function() {
         mongoose.connect('mongodb://' + config.db_config.host + ":" + config.db_config.port + "/" + config.db_config.name)
             .then(() => {
                 console.log('mongo connection created');
+                resolve();
             })
             .catch(err => {
                 console.log('error creating db connection: ' + err);
+                reject();
             });
     });
 };
